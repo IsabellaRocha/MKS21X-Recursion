5dbae46a32d75a57e9d998d4a2f61b5873e3876e
Isabella Rocha <irocha00@stuy.edu>
2019-01-31 16:05:55 -0500

    Added tolerance to sqrt method

--- Recursion.java
+++ Recursion.java
@@ -1,25 +1,25 @@
 public class Recursion {
-  public static double sqrt(double n) {
-    return sqrt(n, 1);
+  public static double sqrt(double n, double tolerance) {
+    return sqrt(n, 1, tolerance);
   }
-  public static double sqrt(double n, double guess) {
-    if (Math.abs(((guess * guess - n) / n)) * 100 <= .0000001) {
+  public static double sqrt(double n, double guess, double tolerance) {
+    if (Math.abs(((guess * guess - n) / n)) * 100 <= tolerance) {
       return guess;
     }
     if (n == 0) {
       return 0;
     }
     else {
-      return sqrt(n, (n/guess + guess) / 2);
+      return sqrt(n, (n/guess + guess) / 2, tolerance);
     }
   }
   public static void main(String[] args) {
-    System.out.println(sqrt(9.0));
-    System.out.println(sqrt(2.0));
-    System.out.println(sqrt(49.0));
-    System.out.println(sqrt(100));
-    System.out.println(sqrt(101));
-    System.out.println(sqrt(1124));
-    System.out.println(sqrt(0));
+    System.out.println(sqrt(9.0, .000001));
+    System.out.println(sqrt(2.0, .000001));
+    System.out.println(sqrt(49.0, .000001));
+    System.out.println(sqrt(100, .000001));
+    System.out.println(sqrt(101, .000001));
+    System.out.println(sqrt(1124, .000001));
+    System.out.println(sqrt(0, .000001));
   }
 }
