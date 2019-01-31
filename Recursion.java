7a54dbea23b88831a069aa598365a748589f2032
Isabella Rocha <irocha00@stuy.edu>
2019-01-31 16:15:14 -0500

    Wrote first version of fib sequence

--- Recursion.java
+++ Recursion.java
@@ -1,8 +1,8 @@
 public class Recursion {
   public static double sqrt(double n, double tolerance) {
-    return sqrt(n, 1, tolerance);
+    return sqrtHelp(n, 1, tolerance);
   }
-  public static double sqrt(double n, double guess, double tolerance) {
+  public static double sqrtHelp(double n, double guess, double tolerance) {
     if (Math.abs(((guess * guess - n) / n)) * 100 <= tolerance) {
       return guess;
     }
@@ -10,8 +10,17 @@ public class Recursion {
       return 0;
     }
     else {
-      return sqrt(n, (n/guess + guess) / 2, tolerance);
+      return sqrtHelp(n, (n/guess + guess) / 2, tolerance);
     }
+  }
+  public static int fib(int n) {
+    return fibHelp(n, 0, 1);
+  }
+  public static int fibHelp(int n, int prev, int prev2) {
+    if (n == 1) {
+      return prev2;
+    }
+    return fibHelp(n, prev2, prev + prev2);
   }
   public static void main(String[] args) {
     System.out.println(sqrt(9.0, .000001));
